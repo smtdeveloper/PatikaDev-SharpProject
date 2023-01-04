18f97819a67073aa93604e00c137976ec6fc1d34
smtdeveloper <sametakcatoki@gmail.com>
2023-01-04 18:58:25 +0300

    Character Reverse Print

---
+++ CharacterReversePrint/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Character Reverse Print");
+
+string input = Console.ReadLine();
+
+
+    string[] strings = input.Split(" ");
+
+    foreach (var word in strings)
+    {
+        char[] chars = word.ToCharArray();
+        Array.Reverse(chars);
+        string reverseWord = new string(chars);
+        Console.Write(reverseWord + " ");
+        break;
+    }
+
+
+
+//for (int i = input.Length - 1; i >= 0; i--)
+//{
+//    Console.Write(input[i]);
+//}
+
+
