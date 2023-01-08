c6f80d1b5469160bfe26f72aab69a0e6ff989197
smtdeveloper <sametakcatoki@gmail.com>
2023-01-09 00:01:32 +0300

    Sessiz Harf

---
+++ SessizHarf/Program.cs
@@ -0,0 +1,30 @@
+﻿//Algoritma
+//Verilen string ifade içerisinde yanyana 2 tane sessiz varsa ekrana true, yoksa false yazdıran console uygulamasını yazınız.
+
+//Örnek: Input: Merhaba Umut Arya
+
+//Output: True False True
+
+
+using System;
+
+while (true)
+{
+    Console.WriteLine("\n\n Boşluk bırakıp kelimelerİ girin");
+
+    foreach (var item in Console.ReadLine().Split())
+    {
+        bool def = false;
+        for (int i = 1; i < item.Length; i++)
+        {
+            if (sessizMi(item[i]) == sessizMi(item[i - 1]))
+                def = true;
+        }
+        Console.Write(def + " ");
+    }
+
+    bool sessizMi(char c)
+    {
+        return !"aeıioöuüAEIİOÖUÜ".ToCharArray().Contains(c);
+    }
+}
