0f6ddce6cc0b5fc4be0de663983dda4d346823aa
smtdeveloper <sametakcatoki@gmail.com>
2023-01-14 00:56:03 +0300

    SerdarOrtacApp

--- CharacterReplacement/Program.cs
+++ CharacterReplacement/Program.cs
@@ -1,24 +1,34 @@
 ﻿using System;
 
-string s = "Merhaba";
-char ilk = s[0];
-char son = s[s.Length - 1];
-s = son + s.Substring(1, s.Length - 2) + ilk;
-Console.WriteLine(s);
+//Algoritma
+//Verilen string ifade içerisindeki karakterleri bir önceki karakter ile yer değiştiren console uygulamasını yazınız.
+
+//Örnek: Input: SMTcoder Hello Question
+
+//Output: rMTcodeS elloH uestionQ
+
+
+string word = "SMTcoder";
+char first = word[0];
+char last = word[word.Length - 1];
+word = last + word.Substring(1, word.Length - 2) + first;
+Console.WriteLine(word);
+
+
 
 Console.WriteLine("-------------------------------------------------");
 
 Console.Write("Karakter Değiştirme : ");
 string input = Console.ReadLine();
 
-char first = input[0];
-char last = input[input.Length - 1];
+char first2 = input[0];
+char last2 = input[input.Length - 1];
 input= last + input.Substring(1, input.Length - 2) + first;
 Console.WriteLine(input);
 
 
 // Tüm kelimeyi cevirme
-string x = "Samet Akca";
+string x = "SMTcoder";
 string reversed = "";
 for (int i = x.Length - 1; i >= 0; i--)
 {
